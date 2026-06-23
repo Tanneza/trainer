cf1ad0ca5e4bd6af8a476fc6119910e0491c0e2f
Tanneza <nezabudka2812@mail.ru>
2026-06-23 21:38:08 +0300

    перенос файлов

--- question_generator.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from question import Question
-
-
-class QuestionGenerator(ABC):
-    @abstractmethod
-    def generate(self) -> list[Question]:
-        pass
--- word.py
+++
@@ -1,7 +0,0 @@
-class Word:
-    def __init__(self, hanzi: str, pinyin: str, tone: int, translations: list[str]):
-        self.hanzi = hanzi
-        self.pinyin = pinyin
-        self.tone = tone
-        self.translations = translations
-
