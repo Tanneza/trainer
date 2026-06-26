1e8f83941a82a8e22bbb676a09684a350a241efb
Tanneza <nezabudka2812@mail.ru>
2026-06-26 22:00:09 +0300

    получение html вопроса

--- backend/services/trainer_service.py
+++ backend/services/trainer_service.py
@@ -23,8 +23,7 @@ def generate_questions_list_by_type(question_type: QuestionType) -> list[int]:
 
 def get_question_by_id(question_id: int) -> str:
     question = question_service.get_question_by_id(question_id)
-    with open(f"question_templates/{question.question_type.value}.html", "r", encoding="utf-8") as file:
-        return file.read()
+    return question.html
 
 
 def check_user_answer(question_id: int, user_answer: str):
