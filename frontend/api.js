10c2e8680612a2a5d0adac349306e069c4d78d84
Dmitry Titov <dmtitov.1983@yandex.ru>
2026-06-26 17:57:12 +0300

    Запросы с фронтенда к API

---
+++ frontend/api.js
@@ -0,0 +1,43 @@
+async function startLesson(questionType) {
+  try {
+    const formData = new FormData();
+    formData.append("question_type", questionType);
+
+    const response = await fetch("/api/lessons", {
+      method: "POST",
+      body: formData,
+    });
+
+    return await response.json();
+  } catch (err) {
+    console.error("Ошибка!", err);
+  }
+}
+
+async function getQuestionById(questionId) {
+  try {
+    const response = await fetch(`/api/questions/${questionId}`, {
+      method: "GET",
+    });
+
+    return await response.json();
+  } catch (err) {
+    console.error("Ошибка!", err);
+  }
+}
+
+async function checkAnswer(questionId, userAnswer) {
+  try {
+    const formData = new FormData();
+    formData.append("user_answer", userAnswer);
+
+    const response = await fetch(`/api/questions/${questionId}/check_answer`, {
+      method: "POST",
+      body: formData,
+    });
+
+    return await response.json();
+  } catch (err) {
+    console.error("Ошибка!", err);
+  }
+}
--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,29 @@
-document.getElementById("send-answer-button").addEventListener("click", (e) => {
-  e.preventDefault();
+async function main() {
+  document.getElementById("send-answer-button").addEventListener("click", async (e) => {
+    e.preventDefault();
 
-  console.log("Нажата кнопка отправки ответа");
-});
+    console.log("Нажата кнопка отправки ответа");
+
+    const userAnswer = document.getElementById("answer-input").value;
+
+    console.log(`User answer = ${userAnswer}`);
+
+    const check_result = await checkAnswer(questionId, userAnswer);
+
+    console.log(`Check result = ${check_result}`);
+  });
+
+  const questionType = "tone_hanzi";
+  const questionsList = await startLesson(questionType);
+
+  console.log(`Questions list = ${questionsList}`);
+
+  const questionId = questionsList[0];
+  const question_html = await getQuestionById(questionId);
+
+  console.log(`Question = ${question_html}`);
+
+  document.getElementById("question-html").innerHTML = question_html;
+}
+
+main();
