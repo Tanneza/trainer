c692a23343e46d701540e6d3360ca86fc90800d3
Dmitry Titov <dmtitov.1983@yandex.ru>
2026-07-07 19:57:11 +0300

    Отображение экрана статистики

--- frontend/js/lessonStatsView.js
+++ frontend/js/lessonStatsView.js
@@ -1,8 +1,14 @@
 import { Component } from "./component.js";
+import { StringTemplate } from "./stringTemplate.js";
 
 export class LessonStatsView extends Component {
   constructor(props) {
     super(props);
+
+    this.questionsCount = props?.questionsCount;
+    this.correctAnswersCount = props?.correctAnswersCount;
+    this.correctAnswersRate = props?.correctAnswersRate;
+    this.roundsCount = props?.roundsCount;
   }
 
   root() {
@@ -11,7 +17,13 @@ export class LessonStatsView extends Component {
 
   async render() {
     const response = await fetch("html/lesson-stats.html");
-    const html = await response.text();
-    return html;
+    const htmlTemplate = await response.text();
+
+    return new StringTemplate(htmlTemplate).fill({
+      questionsCount: this.questionsCount,
+      correctAnswersCount: this.correctAnswersCount,
+      correctAnswersRate: `${(100 * this.correctAnswersRate).toFixed(1)}%`,
+      roundsCount: this.roundsCount,
+    });
   }
 }
