c4ad75facff4179f48e9ed3243950188232b777d
Tanneza <nezabudka2812@mail.ru>
2026-07-16 18:52:29 +0300

    исправлена ошибка в названии переменной

--- backend/services/phrase_dictionary_service.py
+++ backend/services/phrase_dictionary_service.py
@@ -1,14 +1,14 @@
 from generators.phrase_generator import PhraseGenerator
 from models.phrase_dictionary import PhraseDictionary
 
-prase_dictionaries = [
+phrase_dictionaries = [
     "совет дня.csv"
 ]
 
 
 def create_phrase_generator() -> PhraseGenerator:
     phrase_dictionary = PhraseDictionary()
-    for d in prase_dictionaries:
+    for d in phrase_dictionaries:
         phrase_dictionary.load_from_file(d)
     return PhraseGenerator(phrase_dictionary)
 
